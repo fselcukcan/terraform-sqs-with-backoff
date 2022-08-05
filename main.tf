c9566d23b1b53db6877227386bd4019f8a4df108
Faruk Selçuk Can <selcuk_can@epam.com>
2022-08-05 22:05:14 +0200

    just changed queue name prefixes

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_sqs_queue" "sqs" {
-    name = "awesome_co-${var.env}-${var.queue_name}"
+    name = "awesome_new_co-${var.env}-${var.queue_name}"
     visibility_timeout_seconds = var.visibility_timeout
     delay_seconds = 0
     max_message_size = 262144
@@ -9,7 +9,7 @@ resource "aws_sqs_queue" "sqs" {
 }
 
 resource "aws_sqs_queue" "sqs_dead_letter" {
-    name = "awsome_co-${var.env}-${var.queue_name}-dead-letter"
+    name = "awsome_new_co-${var.env}-${var.queue_name}-dead-letter"
     delay_seconds = 0
     max_message_size = 262144
     message_retention_seconds = 1209600 # 14 days.
